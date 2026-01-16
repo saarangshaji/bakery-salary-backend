6f81df02ea040196732f4026e0c1b3b8167843b7
saarang s shaji <117800017+saarangshaji@users.noreply.github.com>
2026-01-16 16:59:34 +0530

    Update db.js

--- src/config/db.js
+++ src/config/db.js
@@ -8,7 +8,7 @@ const pool = new Pool({
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   port: process.env.DB_PORT,
-  ssl: { rejectUnauthorized: false, },
+  ssl: { rejectUnauthorized: false },
 });
 
 (async () => {
